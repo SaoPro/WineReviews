ba12c4bbe7518096a3915d0f811d95ff8d1b9364
SaoPro <96946905+SaoPro@users.noreply.github.com>
2020-11-01 16:32:39 +0000

    Update Plugwoot.js

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,3 +1,3 @@
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
-@REM or more contributor lic
+@REM or more contributor license agreements.  
