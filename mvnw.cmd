f59d15206c6fbfa4a9e53593bc7b1cf30dde3068
SaoPro <96946905+SaoPro@users.noreply.github.com>
2020-11-15 21:18:35 +0000

    changes 2

--- mvnw.cmd
+++ mvnw.cmd
@@ -3,4 +3,5 @@
 @REM or more contributor license agreements.  See the NOTICE file
 @REM distributed with this work for additional information
 @REM regarding copyright ownership.  The ASF licenses this file
-@REM to you under the Apache License, Versi
+@REM to you under the Apache License, Version 2.0 (the
+@
