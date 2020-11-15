db689b6e39195fa01fc7d0c5eeb665f47bcaa245
SaoPro <96946905+SaoPro@users.noreply.github.com>
2020-11-15 12:51:21 +0000

    Delete bin

--- mvnw.cmd
+++ mvnw.cmd
@@ -3,4 +3,4 @@
 @REM or more contributor license agreements.  See the NOTICE file
 @REM distributed with this work for additional information
 @REM regarding copyright ownership.  The ASF licenses this file
-@REM to you un
+@REM to you under the Apache L
