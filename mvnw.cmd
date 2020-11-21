747ffdd30502285618f513f5707096a6b47662e2
SaoPro <96946905+SaoPro@users.noreply.github.com>
2020-11-21 19:47:48 +0000

    jbuilder updated

--- mvnw.cmd
+++ mvnw.cmd
@@ -4,4 +4,4 @@
 @REM distributed with this work for additional information
 @REM regarding copyright ownership.  The ASF licenses this file
 @REM to you under the Apache License, Version 2.0 (the
-@REM "License"); you may not use this file excep
+@REM "License"); you may not use this file except in compli
