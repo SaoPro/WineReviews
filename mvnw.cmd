4cf8d9b6fdb52a231390eba018c801d079f6db97
SaoPro <96946905+SaoPro@users.noreply.github.com>
2020-11-29 09:56:03 +0000

    Prepare to publish

--- mvnw.cmd
+++ mvnw.cmd
@@ -7,4 +7,6 @@
 @REM "License"); you may not use this file except in compliance
 @REM with the License.  You may obtain a copy of the License at
 @REM
-@REM    https://www.apache.org/licenses/LICE
+@REM    https://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Un
