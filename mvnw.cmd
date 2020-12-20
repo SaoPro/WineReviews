40421ccca1fcc4875f61ec6542f4990a826dab18
SaoPro <96946905+SaoPro@users.noreply.github.com>
2020-12-20 07:41:40 +0000

    Bump eslint-utils from 1.4.0 to 1.4.3

--- mvnw.cmd
+++ mvnw.cmd
@@ -11,4 +11,5 @@
 @REM
 @REM Unless required by applicable law or agreed to in writing,
 @REM software distributed under the License is distributed on an
-@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CO
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KI
