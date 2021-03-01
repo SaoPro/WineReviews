1c3a4d152480a25b84271e09154c3b95ec05bcfe
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-03-01 10:59:02 +0000

    moar updates

--- mvnw.cmd
+++ mvnw.cmd
@@ -25,4 +25,5 @@
 @REM
 @REM Optional ENV vars
 @REM M2_HOME - location of maven2's installed home dir
-@REM MAVEN_BATCH_ECHO - set to 'on' to enable the echoing of the batch comm
+@REM MAVEN_BATCH_ECHO - set to 'on' to enable the echoing of the batch commands
+@REM 
