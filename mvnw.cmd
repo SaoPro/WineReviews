7798b7bdc05ec2079500f9643e322109f02eb82d
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-04-04 13:23:21 +0000

    power_supply: lp8788-charger: Fix leaked power supply on probe fail

--- mvnw.cmd
+++ mvnw.cmd
@@ -30,4 +30,4 @@
 @REM MAVEN_OPTS - parameters passed to the Java VM when running Maven
 @REM     e.g. to debug Maven itself, use
 @REM set MAVEN_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000
-@REM MAVEN_SKIP_RC - flag to disable loading of 
+@REM MAVEN_SKIP_RC - flag to disable loading of mavenrc files
