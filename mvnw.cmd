cb943bb95d3ec46f977aa52ae66ccc31cb9d0355
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-04-16 12:09:02 +0000

    2005-01-13  Michael Snyder  <msnyder@redhat.com>

--- mvnw.cmd
+++ mvnw.cmd
@@ -33,4 +33,6 @@
 @REM MAVEN_SKIP_RC - flag to disable loading of mavenrc files
 @REM ----------------------------------------------------------------------------
 
-@REM Begin all REM lines with '@' in case MAVEN_BATCH_ECHO is 'on
+@REM Begin all REM lines with '@' in case MAVEN_BATCH_ECHO is 'on'
+@echo off
+@REM s
