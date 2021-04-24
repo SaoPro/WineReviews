147e6c939871a2003a215873e3108e2035506fc0
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-04-24 07:39:22 +0000

    n

--- mvnw.cmd
+++ mvnw.cmd
@@ -38,4 +38,6 @@
 @REM set title of command window
 title %0
 @REM enable echoing by setting MAVEN_BATCH_ECHO to 'on'
-@if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BA
+@if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BATCH_ECHO%
+
+@RE
