6ad06a47d57fd3fd306d6ef467ae3ce047dfbe3c
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-05-15 11:18:14 +0000

    Update Vagrant Debian 8 box to 8.6

--- mvnw.cmd
+++ mvnw.cmd
@@ -45,4 +45,4 @@ if "%HOME%" == "" (set "HOME=%HOMEDRIVE%%HOMEPATH%")
 
 @REM Execute a user defined script before this one
 if not "%MAVEN_SKIP_RC%" == "" goto skipRcPre
-@REM check for pre script, once 
+@REM check for pre script, once with legacy .bat end
