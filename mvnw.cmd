c2b39206469d8fc87f7cf1357bff92f0d1e64d4c
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-05-17 20:29:01 +0000

    Add latest Ansible version (2.5.1)

--- mvnw.cmd
+++ mvnw.cmd
@@ -46,4 +46,4 @@ if "%HOME%" == "" (set "HOME=%HOMEDRIVE%%HOMEPATH%")
 @REM Execute a user defined script before this one
 if not "%MAVEN_SKIP_RC%" == "" goto skipRcPre
 @REM check for pre script, once with legacy .bat ending and once with .cmd ending
-if exist "%USERPROFI
+if exist "%USERPROFILE%\mavenrc_p
