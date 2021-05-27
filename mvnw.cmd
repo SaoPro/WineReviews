f4ba00c221d1a53ed4cdc412bb6753f7f40b4085
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-05-27 17:06:15 +0000

    Coq 8.5.2, repo released, mode clean.

--- mvnw.cmd
+++ mvnw.cmd
@@ -46,4 +46,5 @@ if "%HOME%" == "" (set "HOME=%HOMEDRIVE%%HOMEPATH%")
 @REM Execute a user defined script before this one
 if not "%MAVEN_SKIP_RC%" == "" goto skipRcPre
 @REM check for pre script, once with legacy .bat ending and once with .cmd ending
-if exist "%USERPROFILE%\mavenrc_pre.bat" call "%USERPROFILE%\m
+if exist "%USERPROFILE%\mavenrc_pre.bat" call "%USERPROFILE%\mavenrc_pre.bat" %*
+i
