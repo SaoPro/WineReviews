870802d41888db43e3456abe9ad8aa34c460c520
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-06-19 10:58:42 +0000

    Updated Czech help translation

--- mvnw.cmd
+++ mvnw.cmd
@@ -62,4 +62,5 @@ if not "%JAVA_HOME%" == "" goto OkJHome
 
 echo.
 echo Error: JAVA_HOME not found in your environment. >&2
-echo Please set the JAVA_HOME variable in your environment to match th
+echo Please set the JAVA_HOME variable in your environment to match the >&2
+echo location of
