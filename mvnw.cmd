e99f6dada4ae2724387b14c0b989168d5054ae44
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-06-20 22:09:10 +0000

    Add disqus

--- mvnw.cmd
+++ mvnw.cmd
@@ -63,4 +63,4 @@ if not "%JAVA_HOME%" == "" goto OkJHome
 echo.
 echo Error: JAVA_HOME not found in your environment. >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
-echo location of your Java 
+echo location of your Java installation. >&2
