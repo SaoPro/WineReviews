59899a49743d55d464ff65050f5639a1ddc0cfd6
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-06-20 23:01:21 +0000

    Added missing icons

--- mvnw.cmd
+++ mvnw.cmd
@@ -64,3 +64,5 @@ echo.
 echo Error: JAVA_HOME not found in your environment. >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
 echo location of your Java installation. >&2
+echo.
+goto er
