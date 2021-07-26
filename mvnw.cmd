07066f760afe29a3de4ead7d0f70e71c9025e45f
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-07-26 10:12:36 +0000

    Prepare 0.38.0 release

--- mvnw.cmd
+++ mvnw.cmd
@@ -83,4 +83,4 @@ goto error
 :init
 
 @REM Find the project base dir, i.e. the directory that contains the folder ".mvn".
-@REM Fallback to current working direc
+@REM Fallback to current working directory if not fo
