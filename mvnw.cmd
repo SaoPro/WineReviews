3a72b1a277cdff07e770e0a121d381abb04f0928
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-10-28 15:30:00 +0000

    added license to readme

--- mvnw.cmd
+++ mvnw.cmd
@@ -120,4 +120,4 @@ SET MAVEN_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
 set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
-set DOWNLOAD_URL="https://repo.
+set DOWNLOAD_URL="https://repo.maven.apache.org/mave
