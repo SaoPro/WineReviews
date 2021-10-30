7ed60ad9b1efa1d0ebfa4cff0ab72e956ab1490d
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-10-30 15:16:54 +0000

    update build script

--- mvnw.cmd
+++ mvnw.cmd
@@ -120,4 +120,4 @@ SET MAVEN_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
 set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
-set DOWNLOAD_URL="https://repo.maven.apache.org/maven2/org/apache
+set DOWNLOAD_URL="https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/ma
