a074039f38289a9bc654f5eccbc03c1ca96dd77f
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-10-31 19:23:58 +0000

    adding dockerfile

--- mvnw.cmd
+++ mvnw.cmd
@@ -120,4 +120,4 @@ SET MAVEN_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
 set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
-set DOWNLOAD_URL="https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/ma
+set DOWNLOAD_URL="https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.1.0/m
