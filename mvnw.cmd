4104527f3bf8cec7ffdb088d77263622ba69c145
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-11-20 19:29:13 +0000

    Update core-java.md

--- mvnw.cmd
+++ mvnw.cmd
@@ -126,4 +126,4 @@ FOR /F "usebackq tokens=1,2 delims==" %%A IN ("%MAVEN_PROJECTBASEDIR%\.mvn\wrapp
     IF "%%A"=="wrapperUrl" SET DOWNLOAD_URL=%%B
 )
 
-@REM Extension to allow automatically downloadi
+@REM Extension to allow automatically downloading the maven-wrappe
