a5b0fb1e772058878fe8afe370b5eb9af887475b
SaoPro <96946905+SaoPro@users.noreply.github.com>
2021-12-18 06:24:07 +0000

    v4.0.3

--- mvnw.cmd
+++ mvnw.cmd
@@ -134,4 +134,4 @@ if exist %WRAPPER_JAR% (
     )
 ) else (
     if not "%MVNW_REPOURL%" == "" (
-        SET DOWNLOAD_URL="%MVNW_REPOURL%/or
+        SET DOWNLOAD_URL="%MVNW_REPOURL%/org/apache/maven
