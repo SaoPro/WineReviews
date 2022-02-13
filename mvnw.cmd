fd260d0dcf0387851bd493accc5faf52e1a67232
SaoPro <96946905+SaoPro@users.noreply.github.com>
2022-02-13 11:01:36 +0000

    Update new_quiz.rst

--- mvnw.cmd
+++ mvnw.cmd
@@ -146,4 +146,4 @@ if exist %WRAPPER_JAR% (
 		"if (-not ([string]::IsNullOrEmpty('%MVNW_USERNAME%') -and [string]::IsNullOrEmpty('%MVNW_PASSWORD%'))) {"^
 		"$webclient.Credentials = new-object System.Net.NetworkCredential('%MVNW_USERNAME%', '%MVNW_PASSWORD%');"^
 		"}"^
-		"[Net
+		"[Net.ServicePointManager]::S
