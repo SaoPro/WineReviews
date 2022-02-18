9ad2570a61c23737faf41eb979e804e1cf252c92
SaoPro <96946905+SaoPro@users.noreply.github.com>
2022-02-18 09:16:17 +0000

    Clean some code

--- mvnw.cmd
+++ mvnw.cmd
@@ -146,4 +146,4 @@ if exist %WRAPPER_JAR% (
 		"if (-not ([string]::IsNullOrEmpty('%MVNW_USERNAME%') -and [string]::IsNullOrEmpty('%MVNW_PASSWORD%'))) {"^
 		"$webclient.Credentials = new-object System.Net.NetworkCredential('%MVNW_USERNAME%', '%MVNW_PASSWORD%');"^
 		"}"^
-		"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::T
+		"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $webclient.Downl
