5bc5cbb8f3cd23a9b2a519f0988a0cbfc8d6441e
SaoPro <96946905+SaoPro@users.noreply.github.com>
2022-03-13 14:47:37 +0000

    Fixed fonts

--- mvnw.cmd
+++ mvnw.cmd
@@ -155,4 +155,4 @@ if exist %WRAPPER_JAR% (
 @REM End of extension
 
 @REM Provide a "standardized" way to retrieve the CLI args that will
-@REM work with both Windows an
+@REM work with both Windows and non-Window
