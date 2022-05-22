d81441cb51dd9b479ed994ccc0f0d675f71f1c20
SaoPro <96946905+SaoPro@users.noreply.github.com>
2022-05-22 11:49:21 +0000

    Remove legacy files

--- mvnw.cmd
+++ mvnw.cmd
@@ -181,4 +181,4 @@ if exist "%USERPROFILE%\mavenrc_post.cmd" call "%USERPROFILE%\mavenrc_post.cmd"
 :skipRcPost
 
 @REM pause the script if MAVEN_BATCH_PAUSE is set to 'on'
-if "%MAVEN_BATCH_PAUSE%"
+if "%MAVEN_BATCH_PAUSE%"=="on" paus
