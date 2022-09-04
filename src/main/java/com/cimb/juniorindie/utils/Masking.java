5d7db08dfea43882c67447a9d9cf12923561fefe
SaoPro <96946905+SaoPro@users.noreply.github.com>
2022-09-04 18:22:30 +0000

    vim updates

--- src/main/java/com/cimb/juniorindie/utils/Masking.java
+++ src/main/java/com/cimb/juniorindie/utils/Masking.java
@@ -19,4 +19,7 @@ public class Masking {
         if(strPOB.length() > 5) {
             strLength = strPOB.substring(0,5);
         }
-        return strLength
+        return strLength;
+    }
+
+    
